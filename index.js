0f511366a3aa27904fe9081f84258cad76ce3abe
Gabriel Martín Blázquez <gmartinbdev@gmail.com>
2020-02-25 23:24:06 +0100

    Add SIGINT signal handling

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const defaultConfig = require('./config');
 const iotAgentOPCUA = require('./src/opcua.iotagent');
 const configService = require('./src/services/config.service');
 
-function start() {
+function main() {
   configService.setConfig(defaultConfig);
-  iotAgentOPCUA.start(defaultConfig.iota, error => {
+  iotAgentOPCUA.start(defaultConfig, error => {
     if (error) {
-      logger.error(
-        'Error starting OPC UA IoT Agents: %s',
-        JSON.stringify(error)
-      );
+      logger.error(`Error starting OPC UA IoT Agent: ${error}`);
+      process.exit(-1);
     } else {
       logger.info('OPC UA IoT Agent started');
+      process.on('SIGINT', () => {
+        iotAgentOPCUA.stop();
+      });
     }
   });
 }
 
-start();
+main();
